43b58678e8b7819a2987f4d3844a716812ff7308
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-02 22:34:04 -0300

    3# Lógica Casteo todo menos Button al cual se lee debe crear el evento onClick

--- Practica_RadioButton15/app/src/main/java/com/example/practica_radiobutton/MainActivity.java
+++ Practica_RadioButton15/app/src/main/java/com/example/practica_radiobutton/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.practica_radiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText et1, et2;
+    private RadioButton rbSumar, rbRestar, rbMultiplicar, rbDividir;
+    //Button onClick!
+    private TextView tvResultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        et1 = (EditText)findViewById(R.id.txt_num1);
+        et2 = (EditText)findViewById(R.id.txt_num2);
+        rbSumar = (RadioButton)findViewById(R.id.rb_sumar);
+        rbRestar = (RadioButton)findViewById(R.id.rb_restar);
+        rbMultiplicar = (RadioButton)findViewById(R.id.rb_multiplicar);
+        rbDividir = (RadioButton)findViewById(R.id.rb_dividir);
     }
 }
