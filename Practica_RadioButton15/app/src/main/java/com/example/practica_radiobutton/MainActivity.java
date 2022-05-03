29173cc3610a09d531619ccbc0d66db257e077ec
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-02 23:25:45 -0300

    8# Valido que los campos no esten vacios

--- Practica_RadioButton15/app/src/main/java/com/example/practica_radiobutton/MainActivity.java
+++ Practica_RadioButton15/app/src/main/java/com/example/practica_radiobutton/MainActivity.java
@@ -30,38 +30,41 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void Calcular (View view){
+    public void Calcular (View view) {
         String valorIngresado1_String = et1.getText().toString();
         String valorIngresado2_String = et2.getText().toString();
 
+        if (et1.getText().toString().isEmpty() || et2.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Debe introducir un valor en ambas casillas", Toast.LENGTH_LONG).show();
+        }else{
 
         int valorIngresado1_Int = Integer.parseInt(valorIngresado1_String);
         int valorIngresado2_Int = Integer.parseInt(valorIngresado2_String);
 
-        if(rbSumar.isChecked() == true){
-          int resultadoSuma_Int = valorIngresado1_Int + valorIngresado2_Int;
-          String resultadoSuma_String = String.valueOf(resultadoSuma_Int);
-          //Muestro resultado
+        if (rbSumar.isChecked() == true) {
+            int resultadoSuma_Int = valorIngresado1_Int + valorIngresado2_Int;
+            String resultadoSuma_String = String.valueOf(resultadoSuma_Int);
+            //Muestro resultado
             tvResultado.setText(resultadoSuma_String);
 
-        }else if(rbRestar.isChecked() == true){
+        } else if (rbRestar.isChecked() == true) {
             int resultadoResta_Int = valorIngresado1_Int - valorIngresado2_Int;
             String resultadoResta_String = String.valueOf(resultadoResta_Int);
             //Muestro resultado
             tvResultado.setText(resultadoResta_String);
 
-        }else if(rbMultiplicar.isChecked() == true){
+        } else if (rbMultiplicar.isChecked() == true) {
             int resultadoMult_Int = valorIngresado1_Int * valorIngresado2_Int;
             String resultadoMult_String = String.valueOf(resultadoMult_Int);
             //Muestro resultado
             tvResultado.setText(resultadoMult_String);
 
-        }else if(rbDividir.isChecked() == true){
-            if(valorIngresado1_Int == 0){
+        } else if (rbDividir.isChecked() == true) {
+            if (valorIngresado1_Int == 0) {
                 Toast.makeText(this, "No se puede dividir por Cero", Toast.LENGTH_LONG).show();
-            }else if (valorIngresado2_Int == 0){
+            } else if (valorIngresado2_Int == 0) {
                 Toast.makeText(this, "El segundo valor debe ser diferente de cero", Toast.LENGTH_LONG).show();
-            }else{
+            } else {
                 int resultadoDiv_Int = valorIngresado1_Int / valorIngresado2_Int;
                 String resultadoDiv_String = String.valueOf(resultadoDiv_Int);
                 //Muestro resultado
@@ -69,5 +72,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
         }
+     }
     }
 }
