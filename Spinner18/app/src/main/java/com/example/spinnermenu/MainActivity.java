4568042df7dac2fbd3f03ea0044388236bf51b87
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-03 15:58:59 -0300

    3# creo Vectro opcionesEsp1 para utilizar en Sprinner

--- Spinner18/app/src/main/java/com/example/spinnermenu/MainActivity.java
+++ Spinner18/app/src/main/java/com/example/spinnermenu/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.spinnermenu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et1, et2;
+    private Spinner esp1;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_valor1);
+        et2 = (EditText)findViewById(R.id.txt_valor2);
+        tv1 = (TextView)findViewById(R.id.textView);
+        esp1 = (Spinner)findViewById(R.id.spinner);
+
+        String opcionesEsp1 [] = {"sumar", "restar", "multiplicar", "dividir"};
     }
 }
