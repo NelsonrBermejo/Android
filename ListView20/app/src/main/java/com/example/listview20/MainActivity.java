e221a4fc477dcefb8aa1fecac28400f02fc02862
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-04 22:25:24 -0300

    6# creo Vector que se encargará de mostrar los nombres .JAVA

--- ListView20/app/src/main/java/com/example/listview20/MainActivity.java
+++ ListView20/app/src/main/java/com/example/listview20/MainActivity.java
@@ -3,9 +3,15 @@ package com.example.listview20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+  private TextView tv1;
+  private ListView lv1;
 
+  //Vector que se encargará de mostrar los nombres
+  private String nombres [] = {"Nelson","Anahi","Matheo","Lo","Fernando","Yanu","Leo","Miel","Fer","Isabel",};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
