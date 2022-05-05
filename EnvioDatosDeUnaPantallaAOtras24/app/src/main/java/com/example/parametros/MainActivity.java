d6a7609c35e019acd13bf5d48b31e7e31519e845
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-05 15:37:42 -0300

    6# creo Metodo utilizando i.putExtra para enviar el dato, en este caso nombre hacia otra actividad

--- EnvioDatosDeUnaPantallaAOtras24/app/src/main/java/com/example/parametros/MainActivity.java
+++ EnvioDatosDeUnaPantallaAOtras24/app/src/main/java/com/example/parametros/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.parametros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText etxNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etxNombre = (EditText)findViewById(R.id.txt_nombre);
+    }
+
+    //Método para el botón enviar dato
+    public void EnviarDato (View view) {
+        Intent i = new Intent(this,MainActivity2.class);
+        //Pasamos por parametros el valor recibido del usuario.
+        i.putExtra("dato",etxNombre.getText().toString());
     }
 }
