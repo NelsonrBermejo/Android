498e1d330d30c246a9888cd83b784edea97c2d54
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-05 16:05:32 -0300

    13# creo metodo para volver al menu principal y poder ingresar otro Nombre o dato

--- EnvioDatosDeUnaPantallaAOtras24/app/src/main/java/com/example/parametros/MainActivity2.java
+++ EnvioDatosDeUnaPantallaAOtras24/app/src/main/java/com/example/parametros/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.parametros;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -18,5 +20,11 @@ public class MainActivity2 extends AppCompatActivity {
 
         String datoNombre = getIntent().getStringExtra("dato");
         tv1.setText("Hola " + datoNombre);
+    }
+
+    //Método Regresar Onclick
+    public  void Regresar(View view){
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
     }
 }
