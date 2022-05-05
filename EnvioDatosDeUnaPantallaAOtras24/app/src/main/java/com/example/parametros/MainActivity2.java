e6c17471d837581e076abfff79eecbb41372fd14
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-05 15:53:41 -0300

    10# cATEO O CREO PUENTE

--- EnvioDatosDeUnaPantallaAOtras24/app/src/main/java/com/example/parametros/MainActivity2.java
+++ EnvioDatosDeUnaPantallaAOtras24/app/src/main/java/com/example/parametros/MainActivity2.java
@@ -3,12 +3,18 @@ package com.example.parametros;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tv1 = (TextView)findViewById(R.id.TextViewHolaNombre);
+        
     }
 }
