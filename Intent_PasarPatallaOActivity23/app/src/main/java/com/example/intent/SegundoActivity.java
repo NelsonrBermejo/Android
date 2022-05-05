29ddc99a6c08608bfb0be5ba40e652dfdb47675a
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-05 14:44:10 -0300

    5# Método para volver  a la pantalla principal

--- Intent_PasarPatallaOActivity23/app/src/main/java/com/example/intent/SegundoActivity.java
+++ Intent_PasarPatallaOActivity23/app/src/main/java/com/example/intent/SegundoActivity.java
@@ -2,7 +2,9 @@ package com.example.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SegundoActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class SegundoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_segundo);
+    }
+
+    //onCLick Método para volver a la primer pantalla
+    public void  Anterior (View view) {
+        Intent anterior = new Intent(this, MainActivity.class);
+        startActivity(anterior);
     }
 }
