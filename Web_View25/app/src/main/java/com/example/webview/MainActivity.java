207c4bd10dcdea0866317fe3c22f64dcf379148e
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-06 15:11:20 -0300

    7# Declaro objeto

--- Web_View25/app/src/main/java/com/example/webview/MainActivity.java
+++ Web_View25/app/src/main/java/com/example/webview/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.webview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText et1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_web);
     }
+
+    
 }
