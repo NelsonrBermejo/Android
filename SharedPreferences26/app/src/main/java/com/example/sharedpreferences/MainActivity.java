5963a8ebd068e4c60bfd151041cd877dce473b61
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-07 10:42:57 -0300

    8# cierro ventana

--- SharedPreferences26/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ SharedPreferences26/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences.Editor editor = preferencias.edit();
         editor.putString("mail",et1Mail.getText().toString());
         editor.commit();
+
+        finish();
     }
 
 }
