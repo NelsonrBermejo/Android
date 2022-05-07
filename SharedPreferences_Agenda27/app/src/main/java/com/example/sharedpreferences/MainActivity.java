5575bb816518ca4ed726ae979266c63fafacd9c3
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-07 12:22:11 -0300

    5# Relación parte lógica-Gráfica y casteo

--- SharedPreferences_Agenda27/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ SharedPreferences_Agenda27/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.sharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et_nombre;
+    private  EditText et_datos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    et_nombre = (EditText)findViewById(R.id.txt_nombre);
+    et_datos = (EditText)findViewById(R.id.txt_datos);
+    //acá se podría agregar el recordatorio de los ultimos datos ingresados
+    //Utilizando SharedPreferences como en la App Anterior
     }
+
+
+
 }
