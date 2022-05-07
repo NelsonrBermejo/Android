eb784efbeec8885d18f6717ca20f4339f0e2015f
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-07 10:26:21 -0300

    3# declaro objeto et1Mail y lo casteo con XML

--- SharedPreferences26/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ SharedPreferences26/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.sharedpreferences;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText et1Mail;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et1Mail = (EditText)findViewById(R.id.EditTextEMail);
+        
     }
 }
