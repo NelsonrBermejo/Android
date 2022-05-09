3da1626086eb1e3dd8cdc7d221dcc4a1b557df9c
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-09 12:57:15 -0300

    el nombre buscado lo seteo en datos y lo muestro

--- SharedPreferences_Agenda27/app/src/main/java/com/example/sharedpreferences/MainActivity.java
+++ SharedPreferences_Agenda27/app/src/main/java/com/example/sharedpreferences/MainActivity.java
@@ -44,6 +44,13 @@ public class MainActivity extends AppCompatActivity {
         String buscarNombre = et_nombre.getText().toString();
         SharedPreferences preferencias = getSharedPreferences("agenda", Context.MODE_PRIVATE);
         String datos = preferencias.getString(buscarNombre,"");
+
+        //si el contacto no existe = Toast de lo contrario lo muestro
+        if(datos.length()==0){
+            Toast.makeText(this, "No se encontro ningún registro", Toast.LENGTH_SHORT).show();
+        } else {
+            et_datos.setText(datos);
+        }
     }
 
 }
