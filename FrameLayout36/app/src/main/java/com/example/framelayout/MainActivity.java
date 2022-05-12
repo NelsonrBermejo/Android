5f5532afecdbee4c4bbd916e847a6e191692c6b7
NelsonrBermejo <bermejonelson.r@gmail.com>
2022-05-12 14:10:12 -0300

    4# Relación lógica-Gráfica

--- FrameLayout36/app/src/main/java/com/example/framelayout/MainActivity.java
+++ FrameLayout36/app/src/main/java/com/example/framelayout/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.framelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private ImageView iv1;
+    private Button bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        iv1 = (ImageView)findViewById(R.id.imageView);
+        bt1 = (Button)findViewById(R.id.button);
     }
 }
